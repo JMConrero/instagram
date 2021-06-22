b83be975b541f82b21e3c386ccc949674553f46a
Tomi_SS <dg.sanchezsoria@gmail.com>
2021-06-22 01:04:10 -0300

    Armado del proyecto

---
+++ my-app/src/components/Header/Header.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import './header.css'
+import profile from '../../img/profile.jpg';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInstagram ,faFacebook, faTwitter } from "@fortawesome/free-brands-svg-icons";
+
+export default function Posts(){
+    return(
+        <>
+        <div className="header-container">
+            <div className="profile">
+                <img src={profile} alt=""/>
+                <div className="info">
+                    <h1 className="name">Ing. Agr. Juan Marcelo Conrero</h1>
+                    <p className="bio">Reformista. Emprendedor. Producto de la Universidad Pública. Agricultor. Decano FCA UNC. Titular Área de Gestión Institucional UNC</p>
+                </div>
+            </div>
+            <div className="social">
+                <p>Seguime tambien en</p>
+                <div className="icons">
+                    <a href="https://www.instagram.com/jmconrero/" target="_blank">
+                    <FontAwesomeIcon icon={faInstagram} />
+                    </a>
+                    <a href="https://www.facebook.com/JMConrero/" target="_blank">
+                    <FontAwesomeIcon icon={faFacebook} />
+                    </a>
+                    <a href="https://twitter.com/conrerojuan" target="_blank">
+                    <FontAwesomeIcon icon={faTwitter} />
+                    </a>
+                </div>
+            </div>
+        </div>
+        </>
+    )
+};
---
+++ my-app/src/components/posts/Posts.jsx
@@ -0,0 +1,62 @@
+import React, {useState, useEffect} from 'react'
+import './post.css'
+import Paginador from '../paginador/Paginador'
+
+
+export default function Posts(){
+    const [postList, setPostList] = useState([])
+    console.log(postList)
+    
+    useEffect(()=>{
+        const api = fetch(
+            `https://api.giphy.com/v1/gifs/search?api_key=zUqJUBhQc8ayRD9ekQbf4JuaKPdkSvU8&q=$campo&limit=12&offset=0&rating=g&lang=es`
+            )
+        .then((response) => {
+            return response.json();
+          })
+          .then((results) => {
+            setPostList(results.data);
+          })
+
+    },[])
+
+    const [pages, setPages] = useState(0);
+    const [current, setCurrent] = useState(0);
+    const size = 4;
+
+    useEffect(()=>{
+        setPages(Math.ceil(postList.length/size));
+        setCurrent(0)
+    },[8])
+
+
+    return(
+        <>
+        <div className="post-container">
+            <p className="title">En los medios</p>
+            {/* esto será un componente */}
+            <div className="layout">
+                <a className="class1"></a>
+            </div>
+            <div className="rest-layout">
+                {postList.map((post)=>{
+                    return(
+                        
+                    <a href={post.url}>
+                     <img src={post.images.original.url} alt={post.title}/>
+                    </a>
+                    )
+                    
+                })}
+                    
+                
+                </div>
+            {/* fin componente */}
+            <Paginador
+                pages={pages}
+                current={current}
+                handlePaginador={setCurrent} />
+        </div>
+        </>
+    )
+};
