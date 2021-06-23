89bce7881d5a2a6d3f306f323d8d7770349a6e23
Tomi_SS <dg.sanchezsoria@gmail.com>
2021-06-23 20:43:09 -0300

    Update post.js
    carga de movilidad virtual

--- my-app/src/post/post.js
+++ my-app/src/post/post.js
@@ -9,11 +9,11 @@ const post = [
         url: "https://bichosdecampo.com/cordoba-tendra-una-escuela-de-comunicacion-agropecuaria-esta-falsa-dicotomia-entre-campo-y-ciudad-fue-instalada-de-forma-premeditada-dice-el-decano-de-agronomia-juan-marcelo-conrero/?fbclid=IwAR0AV7thbGTATNT0pj4ECmxboOe7q4vGyjf-WdhK_xE1vgDJ8bAzVW_qw8I",
         image: "https://agro.unc.edu.ar/uploads/temp/img90.jpeg"    
         },
-     // 003{
-     //     "id": "003",
-     //     "url":"",
-     //     "img": "",
-     // }
+     {
+         id:"003",
+         url:'https://bit.ly/ProgramaMovilidadVirtual',
+         image:'https://scontent.fcor11-1.fna.fbcdn.net/v/t1.6435-9/205111776_4060263930694699_5676410194073689245_n.jpg?_nc_cat=110&ccb=1-3&_nc_sid=730e14&_nc_ohc=ebO32J3aFvkAX9s7lj_&_nc_oc=AQnudyp1WCviDY7Q01JuUVkl5imqxUqIhmMazuDlWojQ1zOTuYdgUCkXvGkPVCM96GO3UMa0cPd7OtxMMmgtkrna&_nc_ht=scontent.fcor11-1.fna&oh=5de7883d35c595349d8619232f1a1718&oe=60D92FAA'
+     }
  ]
  
  export default post;
