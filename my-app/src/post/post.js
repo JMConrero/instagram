2cc3c0fa24d5389890adf9e26fb1a1b8dab4b1c6
Tomi_SS <dg.sanchezsoria@gmail.com>
2021-06-23 12:12:08 -0300

    cambios de url en images

--- my-app/src/post/post.js
+++ my-app/src/post/post.js
@@ -2,13 +2,13 @@ const post = [
      {
          id: "001",
          url:"https://www.clarin.com/rural/ingeniera-cordobesa-creo-empresa-africa-difunde-modelo-produccion-argentino-mundo_0_igXq9cNhh.html",
-         image:'https://images.clarin.com/2021/03/31/haciendo-mediciones-de-altimetria-para___qFRhyeAKJ_1256x620__1.jpg',
+         image:'https://agro.unc.edu.ar/uploads/temp/img91.jpeg',
      },
      {
         id: "002",
         url: "https://bichosdecampo.com/cordoba-tendra-una-escuela-de-comunicacion-agropecuaria-esta-falsa-dicotomia-entre-campo-y-ciudad-fue-instalada-de-forma-premeditada-dice-el-decano-de-agronomia-juan-marcelo-conrero/?fbclid=IwAR0AV7thbGTATNT0pj4ECmxboOe7q4vGyjf-WdhK_xE1vgDJ8bAzVW_qw8I",
-        image: 'https://scontent.fcor11-1.fna.fbcdn.net/v/t1.6435-9/196912784_1370807949969063_4876559471700096136_n.jpg?_nc_cat=104&ccb=1-3&_nc_sid=730e14&_nc_ohc=l2M3zSNnBtEAX-97abu&_nc_ht=scontent.fcor11-1.fna&oh=b741a5d9aa17f7fbd50d2ffb99bed9bf&oe=60D79253',
-    },
+        image: "https://agro.unc.edu.ar/uploads/temp/img90.jpeg"    
+        },
      // 003{
      //     "id": "003",
      //     "url":"",
