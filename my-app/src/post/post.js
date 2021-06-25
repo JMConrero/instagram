4f44f0b0abf3879bc1721901a1a9a459d55f22e3
Tomi_SS <dg.sanchezsoria@gmail.com>
2021-06-25 14:45:15 -0300

    Update post.js
    Agregado colación 2021

--- my-app/src/post/post.js
+++ my-app/src/post/post.js
@@ -13,6 +13,11 @@ const post = [
          id:"003",
          url:'https://bit.ly/ProgramaMovilidadVirtual',
          image:'https://scontent.fcor11-1.fna.fbcdn.net/v/t1.6435-9/205111776_4060263930694699_5676410194073689245_n.jpg?_nc_cat=110&ccb=1-3&_nc_sid=730e14&_nc_ohc=ebO32J3aFvkAX9s7lj_&_nc_oc=AQnudyp1WCviDY7Q01JuUVkl5imqxUqIhmMazuDlWojQ1zOTuYdgUCkXvGkPVCM96GO3UMa0cPd7OtxMMmgtkrna&_nc_ht=scontent.fcor11-1.fna&oh=5de7883d35c595349d8619232f1a1718&oe=60D92FAA'
+     },
+     {
+         id:"004",
+         url:'https://www.youtube.com/watch?v=uOEbm7M4gy0&t=4770s',
+         image: 'https://scontent.fcor11-1.fna.fbcdn.net/v/t1.6435-9/205855651_1385625441820647_5543283920313775562_n.jpg?_nc_cat=111&ccb=1-3&_nc_sid=730e14&_nc_ohc=ILGOEqk4wP8AX8BzFJ-&_nc_oc=AQnNtfrrylMPRjAPVO9f-WLrMJNccGwCy5ARineavty0iKz3kpzJgkhU3uwwyaKObtXHjo2V6Q-AYOGU9Cs_CS7M&_nc_ht=scontent.fcor11-1.fna&oh=5748a0a6ae26be9857e5ed5a702c2964&oe=60DB8317'
      }
  ]
  
