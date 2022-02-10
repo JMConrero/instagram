c34d8d1849148cef4109e1ac9830d2ba69ab79e7
tsanchez-lvi <dg.sanchezsoria@gmail.com>
2022-02-10 10:42:34 -0300

    j

--- my-app/src/post/post.js
+++ my-app/src/post/post.js
@@ -40,10 +40,59 @@ const post = [
         image:"https://i.postimg.cc/gkXL2ckL/image.png"
     },
     {
+        id:"Cadena3 - Analía Ramos",
+        url:"https://www.cadena3.com/noticia/siempre-juntos/doloroso-relato-de-cordobesa-sobre-la-situacion-de-kabul_301605&ori=whatsapp",
+        image:"https://i.postimg.cc/nLy64Zxp/imagen.png"
+    },
+    {
+        id:"CBA24 - Analía Ramos",
+        url:"https://www.cba24n.com.ar/medios/radio-universidad/trabajadora-de-la-onu-sobre-kabul---hemos-retrocedido-20-anos-en-menos-de-un-mes---_a611a85faac5c1274d3010f5b",
+        image:"https://i.postimg.cc/7ZWvL0QC/imagen.png"
+    },
+    {
+        id:"Incendios 1",
+        url:"https://forms.gle/KbV833JjNjz7EoNP6",
+        image:"https://i.postimg.cc/0QFSRtpx/imagen.png"
+    },
+    {
+        id:"Basta incendios",
+        url:"https://forms.gle/KbV833JjNjz7EoNP6",
+        image:"https://i.postimg.cc/rpY0xhZW/imagen.png"
+    },
+    {
         id:"Elecciones UNC",
         url:"https://www.unc.edu.ar/comunicaci%C3%B3n/elecciones-en-la-unc-ya-est%C3%A1-disponible-el-registro-para-que-puedan-inscribirse",
         image:"https://i.postimg.cc/QCLRqWPz/imagen.png"
+    },
+    {
+        id:"Colación Bachiller",
+        url:"https://www.youtube.com/channel/UCY4bEMl8vD3FJ-uBS2zUskA",
+        image:"https://i.postimg.cc/WpSBDrJK/imagen.png"
+    },
+    {
+        id:"DIPLOMATURA EN GESTION DE OPERACIONES AGROINDUSTRIALES",
+        url:"https://www.eventbrite.com.ar/e/diplomatura-en-gestion-de-operaciones-agroindustriales-tickets-166165881625",
+        image:"https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F143694717%2F171982542231%2F1%2Foriginal.20210805-152216?w=800&auto=format%2Ccompress&q=75&sharp=10&rect=0%2C0%2C2160%2C1080&s=289000b04b5102930f8924d82cefaa33"
+    },
+    {
+        id:"Jornadas Integradas 2021",
+        url:"https://jornadasintegradas.wixsite.com/jornadasintegradas",
+        image:"https://i.postimg.cc/Mp82T3KV/imagen.png"
+    },
+    {
+        id:"Premio Campo TEC",
+        url:"https://www.cadena3.com/noticia/siempre-juntos/cadena-3-presenta-el-premio-campo-tec_307449",
+        image:"https://i.postimg.cc/MTgmfDdf/imagen.png"
+    },
+    {
+        id:"Hacia la elección directa",
+        url:"https://www.lavoz.com.ar/ciudadanos/entrevista-a-hugo-juri-vamos-hacia-la-eleccion-directa-de-rector-en-la-unc/",
+        image:"https://www.lavoz.com.ar/resizer/3w6qnVWhy_EP7aeWc7HgT-j-0ak=/1023x682/smart/cloudfront-us-east-1.images.arcpublishing.com/grupoclarin/IABKFHJI5JDGBFGPYJ3S7GAGQ4.jpg"
+    },
+    {
+        id:"Reconversión de las Universidades",
+        url:"https://www.perfil.com/noticias/cordoba/la-reconversion-de-las-universidades-es-una-necesidad-y-una-obligacion.phtml",
+        image:"https://fotos.perfil.com/2021/10/30/trim/950/534/31-10-2021-hugo-juri-1262360.jpg?webp"
     }
  ]
- 
  export default post;
